cf342919578d587c3af87d35e1b5500ca6784d2b
pedrorjccosta <pedrorjccosta@fac.pe.senac.br>
2020-12-18 13:52:38 -0300

    aula concluida

---
+++ js/main.js
@@ -0,0 +1,128 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar!";
+    //console.log(document.getElementById("agradecimento"));
+}
+
+function redirecionar(){
+    window.open("https://twitch.tv/radamanthys2");
+    
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse!"
+    //alert("trocar texto");
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui!"
+}
+
+
+
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    } 
+    else{
+        validar = false
+    }
+    return validar;
+
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+
+
+//alert(soma(5, 10));
+
+*/
+
+
+/*var lista = ["Alemanha", "Inglaterra", "Escócia"];
+console.log(lista.push("Polônia"));
+console.log(lista.pop("Inglaterra"));*/
+
+//var lista = ["mãe", "floresta", "avó"];
+//console.log(lista.join(">"));
+
+
+
+
+/*
+var d = new Date();
+alert(d.getMinutes());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+
+/*
+var count = 0;
+for(count=0; count <=5; count++){
+    alert(count);
+};
+*/
+
+
+/*
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count = count++;
+
+};*/
+
+
+
+/*
+var idade = prompt("Qual sua idade?");
+
+if(idade > 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+} */
+
+
+//var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+//console.log(frutas)
+//alert(frutas[1].nome)
+
+
+
+//var fruta = {nome:"maçã", cor:"vermelha"}
+//console.log(fruta)
+//alert(fruta.cor)
+
+
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString);
+
+
+
+
+//var nome = "Pedro Cunha";
+//var idade = 29;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.toUpperCase());
+//alert(frase.replace("Japão", "Brasil"))
